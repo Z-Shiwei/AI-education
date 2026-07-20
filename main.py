7bd44c7bc64bb1830d68a1fedf36cca28618ac71
Z-Shiwei <96650643+Z-Shiwei@users.noreply.github.com>
2026-07-20 15:02:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -99,6 +99,8 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
